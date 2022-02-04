65e6570085339ad5629caad2e5bd337d1d761501
Wolfgang Vieregg <wollux@me.com>
2022-02-04 22:31:57 +0100

    Update cli_weather.py
    simple changes

--- cli_weather.py
+++ cli_weather.py
@@ -12,7 +12,6 @@ OPENWEATHERMAP_API_METRIC = "metric" #standard, metric and imperial
 OPENWEATHERMAP_API_LANG = "en" #see https://openweathermap.org/current#multi
 
 
-
 def get_weather_data(get_city):
 	request_url = f"{OPENWEATHERMAP_API_URL}?appid={OPENWEATHERMAP_API_KEY}&units={OPENWEATHERMAP_API_METRIC}&lang={OPENWEATHERMAP_API_LANG}&q={get_city}"
 	response = requests.get(request_url)
@@ -24,27 +23,20 @@ def get_weather_data(get_city):
 		#print (f"Request error: "+str(response.status_code))
 		table.add_row(get_city+" / No data",style="red")		
 	
-	
 
-
-
-
-
-
-table = Table(title="OpenWeatherMap - cli_weather.py by Nischenmann")
+table = Table(title="cli_weather.py - data from OpenWeatherMap")
 table.add_column("City/Country", justify="left", style="cyan", no_wrap=True)
 table.add_column("Description", style="yellow")
 table.add_column("Temp", style="blue")
 table.add_column("Humidity", justify="right", style="green")
 
 if len(sys.argv) == 1:	
-	print('Give me a City as arg')
+	print('Please give me the cities where you want to query the weather.')
+	print('for example: cli_weather.py Berlin "New York" Peking')	
 	exit()
 else:
 	for x in sys.argv[1:]:	
-		print(x)
 		get_weather_data(x)
-	
 
 console = Console()
 console.print(table)
